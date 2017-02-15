1aea7a3f4ac512d3e2fe4bb10e780da7914f22b4
leavesster <11785335+leavesster@users.noreply.github.com>
2017-02-15 23:01:10 +0800

    Initial commit

---
+++ Example/NativeWithRNDemo/NativeViewController.m
@@ -0,0 +1,29 @@
+//
+//  NativeViewController.m
+//  NativeWithRNDemo
+//
+//  Created by leavesster on 02/15/2017.
+//  Copyright (c) 2017 leavesster. All rights reserved.
+//
+
+#import "NativeViewController.h"
+
+@interface NativeViewController ()
+
+@end
+
+@implementation NativeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
