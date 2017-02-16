00de775fe83352c6e585ccfdbbfca8fbe57ec4ab
leavesster <a412739861@qq.com>
2017-02-16 22:24:31 +0800

    九宫格视图

--- ReactNativeDemo/index.ios.js
+++ ReactNativeDemo/index.ios.js
@@ -15,18 +15,36 @@ import {
 export default class ReactNativeDemo extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+        <View style={{flex:1, justifyContent:'space-between'}}>
+          <View style={{flex:1, flexDirection:'row'}}>
+            <View style={{flex:1, backgroundColor:'turquoise'}}>
+
+            </View>
+            <View style={{flex:1, backgroundColor:'yellowgreen'}}></View>
+            <View style={{flex:1, backgroundColor:'thistle'}}></View>
+          </View>
+          <View style={{flex:1, flexDirection:'row'}}>
+            <View style={{flex:1, backgroundColor:'slateblue'}}></View>
+            <View style={styles.container}>
+              <Text style={styles.welcome}>
+                Welcome to React Native!
+              </Text>
+              <Text style={styles.instructions}>
+                To get started, edit index.ios.js
+              </Text>
+              <Text style={styles.instructions}>
+                Press Cmd+R to reload,{'\n'}
+                Cmd+D or shake for dev menu
+              </Text>
+            </View>
+            <View style={{flex:1, backgroundColor:'teal'}}></View>
+          </View>
+          <View style={{flex:1,backgroundColor:'firebrick', flexDirection:'row'}}>
+            <View style={{flex:1, backgroundColor:'lightsteelblue'}}></View>
+            <View style={{flex:1, backgroundColor:'orange'}}></View>
+            <View style={{flex:1, backgroundColor:'lightyellow'}}></View>
+          </View>
+        </View>
     );
   }
 }
